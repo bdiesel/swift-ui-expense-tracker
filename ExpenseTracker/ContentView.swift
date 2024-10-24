266658acf0f3acad25f602a57e7e5f55dae7b0ac
Brian Diesel <bri@ndiesel.net>
2024-10-24 11:13:13 -0400

    Add identifiable protocol to expense items

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ExpenseItem {
+struct ExpenseItem: Identifiable {
     let id = UUID()
     let name: String
     let type: String
@@ -26,7 +26,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items, id: \.name) { item in
+                ForEach(expenses.items) { item in
                     Text(item.name)
                 }
                 .onDelete(perform: removeItems)
